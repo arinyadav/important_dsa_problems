a7eaf72914356f2be4786cc866fa7d7df1e40e1e
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2022-11-14 20:33:54 +0530

    Create find_patterns.cpp

---
+++ find_patterns.cpp
@@ -0,0 +1,29 @@
+GFG POTD
+TC-O(N2)
+SC-O(N)
+  
+  
+  
+  class Solution{   
+public:
+    int numberOfSubsequences(string S, string W) {
+        int k;
+        int ans=0;
+        for(int i=0;i<S.size();i++) {
+            if(S[i]==W[0]) {
+                k=0;
+                for(int j=i;j<S.size() && k<W.size();j++) {
+                    if(S[j]==W[k]){
+                        S[j]='@';
+                        k++;
+                    }
+                }
+                if(k==W.size())
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+  
