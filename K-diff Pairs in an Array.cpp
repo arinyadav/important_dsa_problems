6f6fe8caba9adddf1f522e7721ca99d6db598518
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-01-22 21:15:53 +0530

    Update K-diff Pairs in an Array.cpp

--- K-diff Pairs in an Array.cpp
+++ K-diff Pairs in an Array.cpp
@@ -20,3 +20,26 @@ sort(nums.begin(), nums.end());
         }
         return ans;
     }
+
+
+MAPS
+
+
+
+   unordered_map<int,int> a;
+        for(int i:nums)
+            a[i]++;
+        int ans=0;
+        for(auto x:a)
+        {
+            if(k==0)
+            {    
+                if(x.second>1)
+                ans++;
+            }
+             else if(a.find(x.first+k)!=a.end())
+                ans++;
+        }
+        
+        return ans;
+    }
