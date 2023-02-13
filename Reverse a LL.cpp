944336813232e8050cd1c314d62ae3c7aa6d8498
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-02-13 12:37:04 +0530

    Create Reverse a LL.cpp

---
+++ Reverse a LL.cpp
@@ -0,0 +1,14 @@
+    Node* reverse(Node* head) {
+        
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* next = NULL;
+        
+        while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
