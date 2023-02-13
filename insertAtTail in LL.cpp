efe33f768191b09ed00685cfff47bd4ce3244efa
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-02-13 12:47:41 +0530

    Create insertAtTail in LL.cpp

---
+++ insertAtTail in LL.cpp
@@ -0,0 +1,15 @@
+    void insertAtTail(struct Node* &head, struct Node* &tail, int val) {
+        
+        Node* temp = new Node(val);
+        //empty list
+        if(head == NULL) {
+            head = temp;
+            tail = temp;
+            return;
+        }
+        else
+        {
+            tail -> next = temp;
+            tail = temp;
+        }
+    }
