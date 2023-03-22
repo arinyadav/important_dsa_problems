3967473118c35d6a883a088c3b7ad384e6c0762b
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-03-22 08:23:36 +0530

    Update Minimum Score of a Path Between Two Cities.cpp

--- Minimum Score of a Path Between Two Cities.cpp
+++ Minimum Score of a Path Between Two Cities.cpp
@@ -1,3 +1,45 @@
+APPROACH 2 using DFS
+
+class Solution {
+public:
+     void dfs(vector<vector<pair<int, int>>> &adj, int curr, vector<int> &vis, int &mi)
+    {
+        if (vis[curr])
+            return;
+        vis[curr] = 1;
+ 
+        for (auto nxt : adj[curr])
+        {
+            mi = min(mi, nxt.second);
+            dfs(adj, nxt.first, vis, mi);
+        }
+ 
+        return;
+    }
+    int minScore(int n, vector<vector<int>> &roads)
+    {
+        vector<vector<pair<int, int>>> adj(n + 1);
+ 
+        for (int i = 0; i < roads.size(); i++)
+        {
+            adj[roads[i][0]].push_back({roads[i][1], roads[i][2]});
+            adj[roads[i][1]].push_back({roads[i][0], roads[i][2]});
+        }
+ 
+        int mi = INT_MAX;
+        vector<int> vis(n + 1, 0);
+        dfs(adj, 1, vis, mi);
+ 
+        return mi;
+    }
+};
+
+
+
+
+
+
+
 class Solution {
 public:
 typedef long long ll ;
