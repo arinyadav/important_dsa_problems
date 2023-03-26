f6b9b4861a9c8395e3a4066748e58bee389b242a
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-03-26 10:08:14 +0530

    Create Symmetric Tree.cpp

---
+++ Symmetric Tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    bool match(TreeNode* root1, TreeNode* root2){
+        if(root1 != NULL && root2 != NULL){
+            bool a = match(root1->left, root2->right);
+            bool b = match(root1->right, root2->left);
+            
+            if((root1->val == root2->val) && a && b)
+                return true;
+            else
+                return false;
+            
+        }
+        else if(root1 == NULL && root2 == NULL){
+            return true;
+        }else
+            return false;
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL)
+            return true;
+        
+        return match(root->left, root->right);
+    }
+};
