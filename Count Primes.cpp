37920bacc7fd0a9710a8f8afa1c734b651cf8420
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-05-22 13:30:02 +0530

    Create Count Primes(IMP).cpp

---
+++ Count Primes.cpp
@@ -0,0 +1,22 @@
+SIEVE OF ERATOSTENES
+
+
+class Solution {
+public:
+    int countPrimes(int n) {
+
+        int count=0;
+        vector<bool> v(n+1,true);
+
+        v[0]=v[1]=false;
+
+        for(int i=2 ; i<n ; i++) {
+            if(v[i]) {
+                count++;
+                for(int j=2*i ; j<n ;j+=i)
+                v[j]=false;
+            }
+        }
+        return count;
+    }
+};
