02acf235dabfeb61841af2aa2495ff218ce2a702
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-05-22 13:36:00 +0530

    Update Count Primes.cpp

--- Count Primes.cpp
+++ Count Primes.cpp
@@ -11,7 +11,7 @@ public:
         v[0]=v[1]=false;
 
         for(int i=2 ; i<n ; i++) {
-            if(v[i]) {
+            if(v[i]==true) {
                 count++;
                 for(int j=2*i ; j<n ;j+=i)
                 v[j]=false;
