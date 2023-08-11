e65bfd2c6522e89ecd1e7d69b3a64e75469c33e2
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-08-11 13:20:57 +0530

    Update (DFS)Mother Vertex.cpp

--- (DFS)Mother Vertex.cpp
+++ (DFS)Mother Vertex.cpp
@@ -1,22 +1,17 @@
-   void dfs(int V,int front,vector<int> adj[],vector<int> &vis) {
-       vis[front]=1;
-       for(auto i:adj[front]){
-           if(vis[i]==0)
-            dfs(V,i,adj,vis);
-       }
-   }
-  
 	int findMotherVertex(int V, vector<int>adj[])
 	{
 	  vector<int>vis(V,0);
 	  int last_node=0;
 	  
+	  //stroing last node; reaching last node
 	  for(int i=0;i<V;i++) {
 	      if(vis[i]==0){
 	          dfs(V,i,adj,vis);
 	          last_node=i;
 	      }
 	  }
+	  
+	  // doing every zero to check whether from last node we can reach all vertex or not 
 	  for(int i=0;i<V;i++) {
 	      vis[i]=0;
 	  }
