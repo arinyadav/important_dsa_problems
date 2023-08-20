fe0e73db508153b43ae72b7c9b2dfb03a3715374
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-08-20 19:41:53 +0530

    Create (DP) Climbing Stairs.cpp

---
+++ (DP) Climbing Stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int n, vector<int>&dp) {
+        if( n < 0)
+         {
+             return 0;
+         }
+         if( n == 0)
+         {
+             return 1;
+         }
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        dp[n]= solve(n-1,dp)+solve(n-2,dp);
+
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
