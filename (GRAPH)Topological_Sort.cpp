8b2f82b6e8e6bff4c938f939844fda31d16ab94c
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2024-04-18 12:23:16 +0530

    Create (GRAPH)Topological_Sort.cpp

---
+++ (GRAPH)Topological_Sort.cpp
@@ -0,0 +1,26 @@
+	void dfs(int node, int vis[],stack<int>&st, vector<int> adj[]) {
+	    vis[node] = 1;
+	    for(auto it: adj[node]) {
+	        if(!vis[it]) {
+	            dfs(it,vis,st,adj);
+	        }
+	    }
+	    st.push(node);
+	}
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    int vis[V]={0};
+	    stack<int>st;
+	    for(int i=0;i<V;i++) {
+	        if(!vis[i]) {
+	            dfs(i,vis,st,adj);
+	        }
+	    }
+	    vector<int>v;
+	    while(!st.empty()) {
+	        v.push_back(st.top());
+	        st.pop();
+	    }
+	    return v;
+	}
