8302a4796369d679bc3dad7a15130627614046c3
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2024-04-19 11:13:40 +0530

    Create (HEAPS)KthLargest.cpp

---
+++ (HEAPS)KthLargest.cpp
@@ -0,0 +1,27 @@
+//MAX HEAP
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>pq(nums.begin(),nums.end());
+       int ans=0;
+       while(k--) {
+        ans = pq.top();
+        pq.pop();
+       }
+        return ans;
+    }
+};
+
+//MIN HEAP
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>pq(nums.begin(),nums.end());
+       int ans=0;
+       while(k--) {
+        ans = pq.top();
+        pq.pop();
+       }
+        return ans;
+    }
+};
