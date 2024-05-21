f29e75aa3c9a91e18f2603d76ff2229b72a67777
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2024-05-21 07:59:26 +0530

    Update (GRAPH)Topological_Sort.cpp

--- (GRAPH)Topological_Sort.cpp
+++ (GRAPH)Topological_Sort.cpp
@@ -1,4 +1,6 @@
-	void dfs(int node, int vis[],stack<int>&st, vector<int> adj[]) {
+//DFS
+
+void dfs(int node, int vis[],stack<int>&st, vector<int> adj[]) {
 	    vis[node] = 1;
 	    for(auto it: adj[node]) {
 	        if(!vis[it]) {
@@ -23,4 +25,41 @@
 	        st.pop();
 	    }
 	    return v;
+	}
+
+
+//BFS
+
+vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    //get indegree
+	    int id[V] = {0};
+	    for(int i=0;i<V;i++) {
+	        for(auto it: adj[i]) {
+	            id[it]++;
+	        }
+	    }
+	    
+	    //for indegree 0 push in queue
+	    queue<int>q;
+	    for(int i=0;i<V;i++) {
+	        if(id[i]==0) {
+	            q.push(i);
+	        }
+	    }
+	    
+	    //pushing answers
+	    vector<int>topo;
+	    while(!q.empty()) {
+	        int temp = q.front();
+	        q.pop();
+	        topo.push_back(temp);
+	        for(auto it: adj[temp]) {
+	            id[it]--;
+	            if(id[it]==0) {
+	                q.push(it);
+	            }
+	        }
+	    }
+	    return topo;
 	}
